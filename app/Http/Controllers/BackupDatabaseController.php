308e7972275c9cc90c5bf21eeee2a0dc55306403
nabilchen96 <escendol900@gmail.com>
2021-06-13 17:56:13 +0700

    perbaikan

--- app/Http/Controllers/BackupDatabaseController.php
+++ app/Http/Controllers/BackupDatabaseController.php
@@ -8,7 +8,6 @@ use DB;
 class BackupDatabaseController extends Controller
 {
     public function index(){
-        $data = DB::select('SHOW TABLES');
-        return view('backup.index')->with('data', $data);
+        return view('backup.index');
     }
 }
