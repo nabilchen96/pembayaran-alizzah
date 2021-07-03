21494466a89aeae637b922976646259964bd614b
nabilchen96 <escendol900@gmail.com>
2021-07-03 11:50:41 +0700

    perbaikan dan perampingan query

--- app/Http/Controllers/LaporanTunggakanController.php
+++ app/Http/Controllers/LaporanTunggakanController.php
@@ -14,49 +14,71 @@ class LaporanTunggakanController extends Controller
 
     public function json(Request $request){
         //menampilkan semua siswa
-        $data   = DB::table('siswas')->select('id_siswa', 'nis', 'nama_siswa')->get();
+        // $data   = DB::table('siswas')->select('id_siswa', 'nis', 'nama_siswa')->get();
+
+        $data  = DB::table('siswas')
+                        ->join('rombels', 'rombels.id_siswa', '=', 'siswas.id_siswa')
+                        ->join('tahun_ajarans', 'tahun_ajarans.id_tahun', '=', 'rombels.id_tahun')
+                        // ->join('siswas', 'siswas.id_siswa', '=', 'rombels.id_siswa')
+                        ->join('kelas', 'kelas.id_kelas', '=', 'rombels.id_kelas')
+
+                        ->join('set_pembayaran_kelas', 'set_pembayaran_kelas.id_kelas', '=', 'kelas.id_kelas')
+                        ->join('jenis_pembayarans', 'jenis_pembayarans.id_jenis_pembayaran', '=', 'set_pembayaran_kelas.id_jenis_pembayaran')
+                        ->where('jenis_pembayarans.id_jenis_pembayaran',  $request->input('id_jenis_pembayaran'))
+                        // ->where('siswas.id_siswa', $d->id_siswa)
+                        ->where('tahun_ajarans.status_aktif', 1)
+                        ->groupBy('siswas.id_siswa')
+                        ->get();
 
         //mengambil tanggal mulai di tahun aktif
-        $tahun  = DB::table('tahun_ajarans')->select('tgl_mulai')->where('status_aktif', 1)->first();
+        // $tahun  = DB::table('tahun_ajarans')->select('tgl_mulai')->where('status_aktif', 1)->first();
 
+        $jenis_pembayaran = DB::table('jenis_pembayarans')
+                    ->where('id_jenis_pembayaran', $request->input('id_jenis_pembayaran'))
+                    ->first();
         
         foreach($data as $key => $d){
 
             //data kelas
-            $kelas          = DB::table('rombels')
-                                ->join('tahun_ajarans', 'tahun_ajarans.id_tahun', '=', 'rombels.id_tahun')
-                                ->join('siswas', 'siswas.id_siswa', '=', 'rombels.id_siswa')
-                                ->join('kelas', 'kelas.id_kelas', '=', 'rombels.id_kelas')
-                                ->where('siswas.id_siswa', $d->id_siswa)
-                                ->where('tahun_ajarans.status_aktif', 1)
-                                ->first();
+            // $kelas          = DB::table('rombels')
+            //                     ->join('tahun_ajarans', 'tahun_ajarans.id_tahun', '=', 'rombels.id_tahun')
+            //                     ->join('siswas', 'siswas.id_siswa', '=', 'rombels.id_siswa')
+            //                     ->join('kelas', 'kelas.id_kelas', '=', 'rombels.id_kelas')
+
+            //                     ->join('set_pembayaran_kelas', 'set_pembayaran_kelas.id_kelas', '=', 'kelas.id_kelas')
+            //                     ->join('jenis_pembayarans', 'jenis_pembayarans.id_jenis_pembayaran', '=', 'set_pembayaran_kelas.id_jenis_pembayaran')
+            //                     ->where('jenis_pembayarans.id_jenis_pembayaran',  $request->input('id_jenis_pembayaran'))
+            //                     ->where('siswas.id_siswa', $d->id_siswa)
+            //                     ->where('tahun_ajarans.status_aktif', 1)
+            //                     ->first();
 
             //menampilkan pembayaran yang cocok dengan kelas si siswa
-            $spp            = DB::table('rombels')
-                                ->join('siswas', 'siswas.id_siswa', '=', 'rombels.id_siswa')
-                                ->join('kelas', 'kelas.id_kelas', '=', 'rombels.id_kelas')
-                                ->join('set_pembayaran_kelas', 'set_pembayaran_kelas.id_kelas', '=', 'kelas.id_kelas')
-                                ->join('jenis_pembayarans', 'jenis_pembayarans.id_jenis_pembayaran', '=', 'set_pembayaran_kelas.id_jenis_pembayaran')
-                                ->where('siswas.id_siswa', $d->id_siswa)
-                                ->where('jenis_pembayarans.id_jenis_pembayaran',  $request->input('id_jenis_pembayaran'))
-                                ->select('set_pembayaran_kelas.biaya')
-                                ->first();
+            // $spp            = DB::table('rombels')
+            //                     ->join('siswas', 'siswas.id_siswa', '=', 'rombels.id_siswa')
+            //                     ->join('kelas', 'kelas.id_kelas', '=', 'rombels.id_kelas')
+            //                     ->join('set_pembayaran_kelas', 'set_pembayaran_kelas.id_kelas', '=', 'kelas.id_kelas')
+            //                     ->join('jenis_pembayarans', 'jenis_pembayarans.id_jenis_pembayaran', '=', 'set_pembayaran_kelas.id_jenis_pembayaran')
+            //                     ->where('siswas.id_siswa', $d->id_siswa)
+            //                     ->where('jenis_pembayarans.id_jenis_pembayaran',  $request->input('id_jenis_pembayaran'))
+            //                     ->select('set_pembayaran_kelas.biaya')
+            //                     ->first();
 
             //cek apakah menerima keringanan
             $keringanan     = DB::table('penerima_keringanans')
-                                ->join('siswas', 'siswas.id_siswa', '=', 'penerima_keringanans.id_siswa')
+                                // ->join('siswas', 'siswas.id_siswa', '=', 'penerima_keringanans.id_siswa')
                                 ->join('keringanans', 'keringanans.id_keringanan', '=', 'penerima_keringanans.id_keringanan')
                                 ->where('keringanans.id_jenis_pembayaran',  $request->input('id_jenis_pembayaran'))
-                                ->where('siswas.id_siswa', $d->id_siswa)
+                                ->where('penerima_keringanans.id_siswa', $d->id_siswa)
                                 ->first();
 
             //jumlah tunggakan
 
-            $jenis_pembayaran = DB::table('jenis_pembayarans')
-                                ->where('id_jenis_pembayaran', $request->input('id_jenis_pembayaran'))
-                                ->first();
+            // $jenis_pembayaran = DB::table('jenis_pembayarans')
+            //                     ->where('id_jenis_pembayaran', $request->input('id_jenis_pembayaran'))
+            //                     ->first();
 
             if($jenis_pembayaran->total_pembayaran_pertahun == 1){
+
                 $transaksi      = DB::table('transaksis')
                                 ->join('tahun_ajarans', 'tahun_ajarans.id_tahun', '=', 'transaksis.id_tahun')
                                 ->where('id_siswa', $d->id_siswa)
@@ -67,7 +89,7 @@ class LaporanTunggakanController extends Controller
                                 )
                                 ->first();
                 
-                $hutang_tunggakan      = (@$spp->biaya - @$keringanan->besaran_keringanan) - ($transaksi->jumlah_tunggakan);
+                $hutang_tunggakan      = (@$d->biaya - @$keringanan->besaran_keringanan) - ($transaksi->jumlah_tunggakan);
 
                 
             }else{
@@ -78,29 +100,24 @@ class LaporanTunggakanController extends Controller
                                 ->where('id_jenis_pembayaran', $request->input('id_jenis_pembayaran'))
                                 ->where('tahun_ajarans.status_aktif', 1)
                                 ->select(
-                                    // DB::raw('TIMESTAMPDIFF(MONTH, "'.$tahun->tgl_mulai.'", CURRENT_DATE) as total_bulan'),
                                     DB::raw('sum(transaksis.jumlah_bayar) as jumlah_tunggakan')
-                                    // DB::raw('sum(transaksis.jumlah_bayar) as jumlah_tunggakan')
                                 )
                                 ->first();
 
-                $diff           = date_diff( date_create($tahun->tgl_mulai), date_create('m'));
+                $diff           = date_diff( date_create($d->tgl_mulai), date_create('m'));
 
 
-                $hutang_tunggakan    = (@$spp->biaya - @$keringanan->besaran_keringanan) * ($diff->m + 1) - $transaksi->jumlah_tunggakan;
-                // $hutang_tunggakan       = (@spp->biaya - @keringanan->besaran_keringanan);
+                $hutang_tunggakan    = (@$d->biaya - @$keringanan->besaran_keringanan) * ($diff->m + 1) - $transaksi->jumlah_tunggakan;
             }
             
             $siswa[] = array(
                 'id_siswa'          => $d->id_siswa,
                 'nis'               => $d->nis,
                 'nama_siswa'        => $d->nama_siswa,
-                // 'total_tunggakan'   => $transaksi->jumlah_tunggakan + 1,
                 'hutang_tunggakan'  => $hutang_tunggakan,
-                
-                'spp'               => @$spp->biaya,
+                'spp'               => @$d->biaya,
                 'keringanan'        => @$keringanan->besaran_keringanan,
-                'kelas'             => @$kelas->kelas.'/'.@$kelas->jenjang
+                'kelas'             => @$d->kelas.'/'.@$d->jenjang
             );
         }
 
