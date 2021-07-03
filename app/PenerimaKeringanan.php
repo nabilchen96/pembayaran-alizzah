5429aff18d510b48694f6446c0dfa16dbb0f718d
nabilchen96 <escendol900@gmail.com>
2021-07-03 13:26:45 +0700

    ...

--- app/PenerimaKeringanan.php
+++ app/PenerimaKeringanan.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class PenerimaKeringanan extends Model
 {
     protected $fillable = [
-        'id_keringanan', 'id_siswa', 'status_pnerima', 'berkas_keringanan', 'alasan_keringanan'
+        'id_keringanan', 'id_siswa', 'status_penerima', 'berkas_keringanan', 'alasan_keringanan'
     ];
 
     protected $primaryKey = 'id_penerima_keringanan';
